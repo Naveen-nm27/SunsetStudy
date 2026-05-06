546b484fa9d081598f466a05e321dc5eb42d3a7b
Naveen-nm27 <naveenm2795@gmail.com>
2026-05-06 14:04:23 +0530

    connected with mongo db configured .env file

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 import express from "express";
-import mongoose from "express";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 
+dotenv.config();
+const app = express();
+app.use(express.json());
 
+mongoose.connect(process.env.MONGO_URI).then(() => console.log("MongoDB Connected")).catch((err) => console.log("Db Connection",err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server Running on port: ${PORT}`));
